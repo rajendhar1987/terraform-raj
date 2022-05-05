182cb9b522e900b0c5fab2eab2144863cf6774c2
Iswarya Vardhini <iswaryavardhini@gmail.com>
2022-05-04 23:25:31 -0500

    first commit

--- live/nonprod/testapp/network/main.tf
+++ live/nonprod/testapp/network/main.tf
@@ -3,45 +3,6 @@ provider "aws"{
 
 }
 
-variable "vpc_name"{
-    type = string
-
-}
-
-variable "vpc_cidr"{
-    type = string
-    
-}
-
-variable "vpc_azs"{
-    type = list
-    
-}
-
-variable "vpc_private_subnets"{
-    type = list
-    
-}
-
-variable "vpc_public_subnets"{
-    type = list
-    
-}
-
-variable "vpc_enable_nat_gateway"{
-    type = bool
-    
-}
-
-
-variable "vpc_enable_vpn_gateway"{
-    type = bool
-    
-}
-variable "vpc_tags"{
-    type = map
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
